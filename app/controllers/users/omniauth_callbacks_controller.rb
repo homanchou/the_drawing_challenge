a7c5eb03c562ed502cf6e61b70ef6c1dcbf4e6db
Homan Chou <homanchou@gmail.com>
2014-09-28 16:03:17 -0700

    fix missing name column on user model

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
+
+  skip_before_action :verify_authenticity_token
+
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928223243) do
+ActiveRecord::Schema.define(version: 20140928230143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140928223243) do
     t.string   "uid"
     t.json     "oauth"
     t.text     "avatar"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
